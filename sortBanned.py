4dac7cffdd28d14910d6393cac537e4a099f7b6e
ifwlzs <93xo.ox39@gmail.com>
2021-07-28 20:26:37 +0800

    update

---
+++ sortBanned.py
@@ -0,0 +1,14 @@
+l=[]
+path="banned.txt"
+with open(path, 'r') as f:
+    l.append(f.readlines())
+l=l[0]
+for i in range(0, len(l)):
+    l[i] = l[i].strip("\n")
+l.sort()
+print(l)
+with open(path, 'w') as f:
+    for i in l:
+        f.write(i+"\n")
+        f.flush()
+    f.close()
